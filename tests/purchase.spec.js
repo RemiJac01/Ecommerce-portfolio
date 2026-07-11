94ebbfb315108f8009410ce8b0a0f4ad305fa601
RemiJac01 <norburyman@gmail.com>
2026-07-11 13:01:00 +0200

    Add cart and checkout steps to purchase journey test

--- tests/purchase.spec.js
+++ tests/purchase.spec.js
@@ -3,4 +3,7 @@ import { test, expect } from "../fixtures/base.js";
 test("Product purchase journey", async ({ loggedInPage }) => {
   await loggedInPage.goto("https://automationexercise.com/products");
   await loggedInPage.locator('[data-product-id="1"]').first().click();
+  await loggedInPage.getByRole("link", { name: "View Cart" }).click();
+  await loggedInPage.locator("a.check_out").click();
+  await loggedInPage.getByRole("link", { name: "Place Order" }).click();
 });
