c22adfe640c97f7c96aa14dc03ff0e1ff8c41109
RemiJac01 <norburyman@gmail.com>
2026-07-11 09:57:56 +0200

    Add purchase test and loggedInPage fixture

--- tests/purchase.spec.js
+++ tests/purchase.spec.js
@@ -2,5 +2,5 @@ import { test, expect } from "../fixtures/base.js";
 
 test("Product purchase journey", async ({ loggedInPage }) => {
   await loggedInPage.goto("https://automationexercise.com/products");
-  await loggedInPage.locator('[data-product-id="1"]').click();
+  await loggedInPage.locator('[data-product-id="1"]').first().click();
 });
