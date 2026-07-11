4b4abf8682a9593a6a3e9d438cbd94116a3c4e82
RemiJac01 <norburyman@gmail.com>
2026-07-11 16:37:42 +0200

    Complete purchase journey test with payment and confirmation assertions

--- tests/purchase.spec.js
+++ tests/purchase.spec.js
@@ -6,5 +6,18 @@ test("Product purchase journey", async ({ loggedInPage }) => {
   await loggedInPage.getByRole("link", { name: "View Cart" }).click();
   await loggedInPage.locator("a.check_out").click();
   await loggedInPage.getByRole("link", { name: "Place Order" }).click();
-  await loggedInPage.locator("[]");
+  await loggedInPage.locator('[data-qa="name-on-card"]').fill("PWTest");
+  await loggedInPage.locator('[data-qa="card-number"]').fill("1234-5678-91011");
+  await loggedInPage.locator('[data-qa="cvc"]').fill("235");
+  await loggedInPage.locator('[data-qa="expiry-month"]').fill("12");
+  await loggedInPage.locator('[data-qa="expiry-year"]').fill("2030");
+  await loggedInPage
+    .getByRole("button", { name: "Pay and Confirm Order" })
+    .click();
+  await expect(loggedInPage).toHaveURL(
+    "https://automationexercise.com/payment_done/500",
+  );
+  await expect(
+    loggedInPage.getByText("Congratulations! Your order has been confirmed!"),
+  ).toBeVisible();
 });
