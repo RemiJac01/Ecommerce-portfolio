f33b6fe5014c19a3bcb56a3a87b205841ef04b93
RemiJac01 <norburyman@gmail.com>
2026-07-19 16:33:07 +0200

    Add data driven negative login tests

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,6 +1,34 @@
 import { test, expect } from "../fixtures/base.js";
 import { LoginPage } from "../pages/LoginPage.js";
 
+const invalidLogins = [
+  {
+    username: "incorrect@email.com",
+    password: "PW123",
+    error: "Your email or password is incorrect!",
+  },
+  {
+    username: "PWtest@PW.com",
+    password: "PW123456",
+    error: "Your email or password is incorrect!",
+  },
+];
+
+for (const arrayData of invalidLogins) {
+  test(`Incorrect UN & PW tests for ${arrayData.username}`, async ({
+    page,
+  }) => {
+    await page.goto("https://automationexercise.com/login");
+    const consentButton = page.getByRole("button", { name: "Consent" });
+    if (await consentButton.isVisible()) {
+      await consentButton.click();
+    }
+    const signInPage = new LoginPage(page);
+    await signInPage.login(arrayData.username, arrayData.password);
+    await expect(page.getByText(arrayData.error)).toBeVisible();
+  });
+}
+
 test("Login", async ({ page }) => {
   await page.goto("https://automationexercise.com/login");
   const consentButton = page.getByRole("button", { name: "Consent" });
